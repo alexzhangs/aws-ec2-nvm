9944348c3d50cea7313bd7dabf9854e0b94fee08
alex <alexzhangs@gmail.com>
2016-07-23 14:49:48 +0800

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # aws-ec2-nvm
+
 Install nvm(Node Version Manager) on AWS EC2 instance
+
+## Installation
+
+```
+git clone https://github.com/alexzhangs/aws-ec2-nvm.git
+sudo sh aws-ec2-nvm/install.sh
+```
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+usage () {
+    printf "Usage: ${0##*/} [-v VERSION | -h]\n"
+    printf "OPTIONS\n"
+    printf "\t[-v VERSION]\n\n"
+    printf "\tnvm version, default is 0.31.3.\n\n"
+    printf "\t[-h]\n\n"
+    printf "\tThis help.\n\n"
+    exit 255
+}
+
+while getopts v:h opt; do
+    case $opt in
+        v)
+            VERSION=$OPTARG
+            ;;
+        h|*)
+            usage
+            ;;
+    esac
+done
+
+[[ -z $VERSION ]] && VERSIION="0.31.3"
+
+SCRIPT="https://raw.githubusercontent.com/creationix/nvm/v$VERSION/install.sh"
+
+curl -o- "$SCRIPT" | bash
+exit $?
